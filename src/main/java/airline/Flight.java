3046adf2c1a4f792577250d5046e5dad64631051
Vilte N <viltenor@gmail.com>
2022-08-31 10:00:52 +0100

    updated CLI

--- src/main/java/airline/Flight.java
+++ src/main/java/airline/Flight.java
@@ -9,8 +9,9 @@ public class Flight {
   private int flightID;
 
   private ArrayList<Passenger> flightPassengers;
-
-  public Flight(String destination, int flightID){
+  private Passenger passenger;
+  public Flight()//String destination, int flightID)
+  {
     this.destination = destination;
     this.flightID = flightID;
     this.flightPassengers = new ArrayList<>();
--- src/main/java/airline/Main.java
+++ src/main/java/airline/Main.java
@@ -1,8 +1,10 @@
 package airline;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+/*
 Flight flight1 = new Flight("Madrid",1);
 Flight flight2=new Flight("London",2);
 Passenger passenger1 = new Passenger("Bob", "Bob@gmail.com", 1);
@@ -26,29 +28,41 @@ Airline airline2 = new Airline("EasyJet");
 flight1.bookPassenger(passenger1);
 passenger1.addFlightBooking(flight1);
         System.out.println(passenger1.getFlightBooking());
-
-
+*/
+        Airline airline1 = new Airline("Ryanair");
         // Start of CLI - Attempt
+
+        Scanner scan = new Scanner(System.in);
+        Flight flight1 = new Flight();
+
+        System.out.println("Hi! Welcome to the VFK app. Please type: Add a flight");
+
+        String answer1 = scan.nextLine();
+
+        if (answer1.equals("Add a flight")){
+            System.out.println("Where is your destination?");
+            String destination = scan.nextLine();
+            flight1.setDestination(destination);
+            System.out.println("What is the flight ID?");
+            int flightID= scan.nextInt();
+            flight1.setFlightID(flightID);
+            System.out.println(flight1.getDestination());
+            System.out.println(flight1.getFlightID());
+            airline1.addNewFlight(flight1);
+            System.out.println(airline1.displayAvailFlights());
+
+        }
+          else if (answer1.equals("Display all flights")){
+              airline1.displayAvailFlights();
 //
-//        Scanner scan = new Scanner(System.in);
-//
-//        System.out.println("Hi! Welcome to the VFK app. Please choose from the following options: Add a flight, Display all flights, Add a passenger");
-//
-//        String answer1 = scan.next();
-//
-//        if (answer1.equals("Add a flight")){
-//              System.out.println("Where is your destination?");}
-//          else if (answer1.equals("Display all flights")){
-//              airline1.displayAvailFlights();
-//              airline2.displayAvailFlights();
-//            }
-//          else if (answer1.equals("Add a passenger")){
-//                System.out.println("What is passengers first name?");
-//            }
-//          else {
-//                System.out.println("Please correctly type out one of the options given above.");
-//            }
-//        }
+            }
+          else if (answer1.equals("Add a passenger")){
+                System.out.println("What is passengers first name?");
+            }
+          else {
+                System.out.println("Please correctly type out one of the options given above.");
+            }
+        }
 
 
 
@@ -56,4 +70,4 @@ passenger1.addFlightBooking(flight1);
 
 // end of code
     }
-}
+
